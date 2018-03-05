6c5df0aa81dacae1cef5e47109422e05d030fa46
AmiBell <miratou96@hotmail.com>
2018-03-05 19:31:42 +0100

    comiitttt

--- app/src/main/java/com/example/aspirev/myproject/MainActivity.java
+++ app/src/main/java/com/example/aspirev/myproject/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.aspirev.myproject;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,16 +15,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private static final String TAG = "MainActivity";
+    private static  final int ERROR_DIALOG_REQUEST = 9001 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if(isServiceOk()){
+            init();
+        }
 
 
 
@@ -115,4 +126,41 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    private  void init(){
+
+
+            }
+
+
+
+
+
+
+    protected boolean isServiceOk(){
+        Log.d(TAG,"isServiceOk:cheking google service version ");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(available== ConnectionResult.SUCCESS)
+        {
+            Log.d(TAG,"isServiceOk: google service is working  ");
+            return true ;
+        }else
+        if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
+        {
+
+            Log.d(TAG,"isServiceOk: gan error occured but we can fix it  ");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+
+        } else {
+            Toast.makeText(this,"you can't make map request",Toast.LENGTH_SHORT).show();
+
+        }
+        return false  ;
+    }
+
+
+
+
+
+
 }
