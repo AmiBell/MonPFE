52e42bebdbf205d124a9b6a0936b9cd80593ffbe
lenovo <soniahassouna1@gmail.com>
2018-03-06 13:28:53 +0000

    BDD

--- app/src/main/java/com/example/aspirev/myproject/MainActivity.java
+++ app/src/main/java/com/example/aspirev/myproject/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
     private static  final int ERROR_DIALOG_REQUEST = 9001 ;
+
+
+    DataBaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +38,6 @@ public class MainActivity extends AppCompatActivity
             init();
         }
 
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -45,6 +46,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        myDb = new DataBaseHelper(this);
     }
 
     @Override
